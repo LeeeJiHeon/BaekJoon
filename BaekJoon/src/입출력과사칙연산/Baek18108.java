6024c615ac5fbf9896113e7c4fd67c5ada138587
LeeJiHeon <dlwlflgjs@naver.com>
2024-01-11 20:49:24 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ BaekJoon/src/입출력과사칙연산/Baek18108.java
@@ -0,0 +1,14 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class Baek18108 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		System.out.println(y - 543);
+	}
+
+}
