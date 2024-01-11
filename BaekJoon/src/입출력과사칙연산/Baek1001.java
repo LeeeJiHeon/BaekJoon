a880329255698a8700c8f260a71bdda3a7995a2b
LeeJiHeon <dlwlflgjs@naver.com>
2024-01-11 19:36:19 +0900

    A - B

---
+++ BaekJoon/src/입출력과사칙연산/Baek1001.java
@@ -0,0 +1,15 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class Baek1001 {
+	public static void main(String argsp[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.print(A - B);
+	}
+
+}
